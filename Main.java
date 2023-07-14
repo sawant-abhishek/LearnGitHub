e8c7dea446450e3d60d2e89007d2693368fc6e1d
Abhishek Sawant <sawantabhishek022002@gmail.com>
2023-07-14 19:32:35 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[ args]){
+        //int[] arr = new int[5];
+        //int[] arr1 = {23,14,13,31,}
+
+        int[] arr; //declaration
+        arr = new int[5]; // initialisation
+    }
+}
